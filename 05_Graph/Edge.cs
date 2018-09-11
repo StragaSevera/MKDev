2f2d0168147310ab90e1960ad15c93b250768fe2
StragaSevera <ought@yandex.ru>
2018-09-11 12:45:40 +0300

    Add vertexes and edges

---
+++ 05_Graph/Edge.cs
@@ -0,0 +1,16 @@
+﻿namespace _05_Graph
+{
+    internal class Edge<T>
+    {
+        public T Value { get; set; }
+        public Vertex<T> StartVertex { get; }
+        public Vertex<T> EndVertex { get; }
+
+        public Edge(Vertex<T> startVertex, Vertex<T> endVertex, T value = default(T))
+        {
+            Value = value;
+            StartVertex = startVertex;
+            EndVertex = endVertex;
+        }
+    }
+}
---
+++ 05_Graph/Vertex.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Graph
+{
+    internal class Vertex<T>
+    {
+        public T Value { get; set; }
+        public IList<Edge<T>> Edges { get; }
+
+        public Vertex(T value = default(T))
+        {
+            Value = value;
+            Edges = new List<Edge<T>>();
+        }
+
+        public void AddEdge(Vertex<T> target, T edgeValue = default(T))
+        {
+            var edge = new Edge<T>(this, target, edgeValue);
+            Edges.Add(edge);
+        }
+
+        public void AddEdgeBidirectional(Vertex<T> target, T edgeValue = default(T))
+        {
+            AddEdge(target, edgeValue);
+            target.AddEdge(this, edgeValue);
+        }
+
+        public void RemoveEdge(Vertex<T> target)
+        {
+            var edge = Edges.FirstOrDefault(e => e.EndVertex == target);
+            if (edge == null)
+            {
+                throw new ArgumentException("Trying to remove an edge which do not exist");
+            }
+
+            Edges.Remove(edge);
+        }
+
+        public void RemoveEdgeBidirectional(Vertex<T> target)
+        {
+            RemoveEdge(target);
+            target.RemoveEdge(this);
+        }
+    }
+}
