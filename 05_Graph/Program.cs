994d4b9aeedea8dc03888a3f8a1e8d667093beb9
StragaSevera <ought@yandex.ru>
2018-09-11 15:42:28 +0300

    Implement salesman nearest friend

--- 05_Graph/Program.cs
+++ 05_Graph/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using _05_Graph.TraversingStrategies;
@@ -29,10 +30,11 @@ namespace _05_Graph
             Console.WriteLine($"Second edge: {graph[1][0]}");
 
             Console.WriteLine("\nUsing depth first strategy:");
-            TraversingStrategy<string, int> strategy = new DepthFirstTraversingStrategy<string, int>();
+            TraversingStrategy<string, int> strategy =
+                new DepthFirstTraversingStrategy<string, int>();
             strategy.VertexEnter += (o, i) =>
                 Console.WriteLine($"Reached vertex {i.Vertex} by #{i.Edge} edge");
-            
+
             Console.WriteLine("Traversing from first vertex:");
             graph.Traverse(strategy, graph[0]);
 
@@ -49,6 +51,31 @@ namespace _05_Graph
 
             Console.WriteLine("Traversing from second vertex:");
             graph.Traverse(strategy, graph[1]);
+
+            var salesmanGraph = new Graph<string, double>();
+            var towns = new List<(Vertex<string, double>, Vector2)>
+            {
+                (salesmanGraph.AddVertex("A"), new Vector2(0, 0)),
+                (salesmanGraph.AddVertex("B"), new Vector2(1, 2)),
+                (salesmanGraph.AddVertex("C"), new Vector2(3, 5)),
+                (salesmanGraph.AddVertex("D"), new Vector2(5, 1)),
+                (salesmanGraph.AddVertex("E"), new Vector2(-1, 3))
+            };
+            var salesmanStrategy = new SalesmanNearestFriend(salesmanGraph, towns);
+            salesmanStrategy.AddEdgesByTown();
+            var salesmanResult = salesmanStrategy.Travel(towns[0].Item1).ToList();
+            double length = 0d;
+            for (int i = 0; i < salesmanResult.Count; i++)
+            {
+                var vertex = salesmanResult[i];
+                var nextVertex = i < salesmanResult.Count - 1
+                    ? salesmanResult[i + 1]
+                    : salesmanResult[0];
+                var edge = vertex.GetEdgeTo(nextVertex);
+                length += edge.Value;
+                Console.WriteLine($"From {vertex.Value} to {nextVertex.Value}: {edge.Value} " +
+                                  $"({length})");
+            }
 
             Console.ReadLine();
         }
---
+++ 05_Graph/SalesmanNearestFriend.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Graph
+{
+    internal class SalesmanNearestFriend
+    {
+        private readonly Graph<string, double> _graph;
+        private readonly List<(Vertex<string, double>, Vector2)> _towns;
+
+        public SalesmanNearestFriend(Graph<string, double> graph,
+            List<(Vertex<string, double>, Vector2)> towns)
+        {
+            _graph = graph;
+            _towns = towns;
+        }
+
+        public void AddEdgesByTown()
+        {
+            for (int i = 0; i < _towns.Count; i++)
+            {
+                for (int j = i + 1; j < _towns.Count; j++)
+                {
+                    double distance = Vector2.Distance(_towns[i].Item2, _towns[j].Item2);
+                    _graph[i].AddEdgeToBidirectional(_graph[j], distance);
+                }
+            }
+        }
+
+        public IEnumerable<Vertex<string, double>> Travel(Vertex<string, double> start)
+        {
+            var result = new List<Vertex<string, double>> {start};
+            while (result.Count < _graph.Vertices.Count)
+            {
+                var lastVertex = result.Last();
+                var edges = lastVertex.Edges.Where(e => !result.Contains(e.EndVertex));
+                var nearestEdge = edges.OrderBy(e => e.Value).First();
+                result.Add(nearestEdge.EndVertex);
+            }
+
+            return result;
+        }
+    }
+}
