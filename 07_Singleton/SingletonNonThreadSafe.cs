547f6d60f2ecdc14f6f8b36d4cc0f79f33889a15
StragaSevera <ought@yandex.ru>
2018-09-27 17:21:19 +0300

    Implement Singletons

---
+++ 07_Singleton/SingletonNonThreadSafe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Singleton
+{
+    internal class SingletonNonThreadSafe
+    {
+        private static SingletonNonThreadSafe _instance;
+
+        public static SingletonNonThreadSafe Instance => _instance ?? 
+                                                         (_instance = new SingletonNonThreadSafe());
+
+        private SingletonNonThreadSafe()
+        {
+        }
+    }
+}
---
+++ 07_Singleton/SingletonThreadSafe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Singleton
+{
+    internal class SingletonThreadSafe
+    {
+        private static readonly Lazy<SingletonThreadSafe> _lazy =
+            new Lazy<SingletonThreadSafe>(() => new SingletonThreadSafe());
+
+        public static SingletonThreadSafe Instance => _lazy.Value;
+
+        private SingletonThreadSafe()
+        {
+        }
+    }
+}
